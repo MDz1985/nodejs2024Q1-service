b6623da6483ed055fe27563e82b88e6c05a23e9f
MDz1985 <m_d@mail.uk>
2024-03-11 15:54:26 +0100

    fix: update swagger doc for tracks

---
+++ src/tracks/common/enums/errors.enum.ts
@@ -0,0 +1,6 @@
+export enum TRACK_ERRORS {
+  INVALID_ID = 'Track id is invalid',
+  TRACK_DOESNT_EXIST = 'Track with this id does not exist',
+  NOT_ALL_FIELDS = 'Not all required fields were provided',
+  WRONG_OLD_PASSWORD = 'Wrong old password',
+}
--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -11,6 +11,9 @@ import {
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiParam,
@@ -24,9 +27,11 @@ import { validate } from 'uuid';
 import { Response } from 'express';
 import { CreateTrackDto } from './common/dto/create-track.dto';
 import { UpdateTrackDto } from './common/dto/update-track.dto';
+import { TRACK_ERRORS } from './common/enums/errors.enum';
 
 @ApiTags('tracks')
 @Controller('track')
+@ApiInternalServerErrorResponse({ description: 'Internal server error' })
 export class TracksController {
   constructor(private readonly tracksService: TracksService) {}
 
@@ -40,54 +45,28 @@ export class TracksController {
   @Get(':id')
   @ApiOperation({ summary: 'Get single track by id' })
   @ApiParam({ name: 'id', description: 'Track ID' })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description: 'Return the record with provided id',
   })
-  @ApiResponse({ status: 400, description: 'Invalid ID - Not a valid UUID' })
-  @ApiResponse({
-    status: 404,
-    description: 'Record with this id does not exist',
+  @ApiBadRequestResponse({ description: TRACK_ERRORS.INVALID_ID })
+  @ApiNotFoundResponse({
+    description: TRACK_ERRORS.TRACK_DOESNT_EXIST,
   })
   async getTrackById(
     @Param('id') trackId: string,
     @Res({ passthrough: true }) res: Response,
   ): Promise<Track | { error: string }> {
     if (!validate(trackId)) {
-      res.status(StatusCodes.BAD_REQUEST).send({ error: 'Invalid trackId' });
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: TRACK_ERRORS.INVALID_ID });
       return;
     }
     const track: Track = await this.tracksService.getTrackById(trackId);
     if (!track) {
-      res.status(StatusCodes.NOT_FOUND).send({ error: 'Track not found' });
-      return;
-    }
-    return track;
-  }
-
-  @Get(':id/artist')
-  @ApiOperation({ summary: 'Get single track by id' })
-  @ApiParam({ name: 'id', description: 'Track ID' })
-  @ApiResponse({
-    status: 200,
-    description: 'Return the record with provided id',
-  })
-  @ApiResponse({ status: 400, description: 'Invalid ID - Not a valid UUID' })
-  @ApiResponse({
-    status: 404,
-    description: 'Record with this id does not exist',
-  })
-  async getArtistByTrackId(
-    @Param('id') trackId: string,
-    @Res() res: Response,
-  ): Promise<Track | { error: string }> {
-    if (!validate(trackId)) {
-      res.status(StatusCodes.BAD_REQUEST).send({ error: 'Invalid trackId' });
-      return;
-    }
-    const track: Track = await this.tracksService.getTrackById(trackId);
-    if (!track) {
-      res.status(StatusCodes.NOT_FOUND).send({ error: 'Track not found' });
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .send({ error: TRACK_ERRORS.TRACK_DOESNT_EXIST });
       return;
     }
     return track;
@@ -95,10 +74,8 @@ export class TracksController {
 
   @Post()
   @ApiOperation({ summary: 'Create track' })
-  @ApiResponse({ status: 201, description: 'Track created' })
-  @ApiResponse({ status: 400, description: 'Invalid data' })
-  @ApiResponse({ status: 409, description: 'Track already exists' })
-  @ApiResponse({ status: 500, description: 'Internal server error' })
+  @ApiCreatedResponse({ description: 'Track created' })
+  @ApiBadRequestResponse({ description: TRACK_ERRORS.TRACK_DOESNT_EXIST })
   async createTrack(
     @Body() dto: CreateTrackDto,
     @Res({ passthrough: true }) res: Response,
@@ -110,7 +87,9 @@ export class TracksController {
       Object.keys(dto).length > 4 ||
       typeof dto?.name !== 'string'
     ) {
-      res.status(StatusCodes.BAD_REQUEST).send({ error: 'Invalid data' });
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: TRACK_ERRORS.TRACK_DOESNT_EXIST });
       return;
     }
     res
@@ -122,19 +101,13 @@ export class TracksController {
   @Put(':id')
   @ApiOperation({ summary: 'Update track info' })
   @ApiOkResponse({
-    status: HttpStatus.OK,
     description: 'Return the updated track',
     type: Track,
   })
   @ApiBadRequestResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Invalid trackId',
+    description: TRACK_ERRORS.INVALID_ID,
   })
-  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Track not found' })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Internal server error',
-  })
+  @ApiNotFoundResponse({ description: TRACK_ERRORS.TRACK_DOESNT_EXIST })
   async updateTrackInfo(
     @Param('id') trackId: string,
     @Body() updateTrackDto: UpdateTrackDto,
@@ -145,12 +118,16 @@ export class TracksController {
       !updateTrackDto?.name ||
       !updateTrackDto?.duration
     ) {
-      res.status(HttpStatus.BAD_REQUEST).send({ error: 'Invalid trackId' });
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ error: TRACK_ERRORS.INVALID_ID });
       return;
     }
     const track = await this.tracksService.getTrackById(trackId);
     if (!track) {
-      res.status(HttpStatus.NOT_FOUND).send({ error: 'Track not found' });
+      res
+        .status(HttpStatus.NOT_FOUND)
+        .send({ error: TRACK_ERRORS.TRACK_DOESNT_EXIST });
       return;
     }
     return await this.tracksService.updateTrack(trackId, updateTrackDto);
@@ -160,25 +137,24 @@ export class TracksController {
   @ApiOperation({ summary: 'Delete track' })
   @ApiOkResponse({ status: HttpStatus.OK, description: 'Track deleted' })
   @ApiBadRequestResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Invalid trackId',
+    description: TRACK_ERRORS.INVALID_ID,
   })
-  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Track not found' })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Internal server error',
-  })
+  @ApiNotFoundResponse({ description: TRACK_ERRORS.TRACK_DOESNT_EXIST })
   async deleteTrack(
     @Param('id') trackId: string,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
     if (!validate(trackId)) {
-      res.status(HttpStatus.BAD_REQUEST).send({ error: 'Invalid trackId' });
+      res
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ error: TRACK_ERRORS.INVALID_ID });
       return;
     }
     const track = await this.tracksService.getTrackById(trackId);
     if (!track) {
-      res.status(HttpStatus.NOT_FOUND).send({ error: 'Track not found' });
+      res
+        .status(HttpStatus.NOT_FOUND)
+        .send({ error: TRACK_ERRORS.TRACK_DOESNT_EXIST });
       return;
     }
     await this.tracksService.deleteTrack(trackId);
