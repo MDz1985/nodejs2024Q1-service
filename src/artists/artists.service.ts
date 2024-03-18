79e368501dca07ae2781e15509ba31e299e22063
MDz1985 <m_d@mail.uk>
2024-03-18 12:36:35 +0100

    feat: connect artist methods to database

--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -2,34 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { Artist } from './common/interfaces/artist.interface';
 import { CreateArtistDto } from './common/dto/create-artist.dto';
 import { UpdateArtistDto } from './common/dto/update-artist.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ArtistsService {
-  private _artists: Artist[] = [];
+  // private _artists: Artist[] = [];
 
+  constructor(private readonly _prisma: PrismaService) {}
   async getAllArtists(): Promise<Artist[]> {
-    return this._artists;
+    return this._prisma.artist.findMany();
+    // return this._artists;
   }
 
   async getArtistById(id: string): Promise<Artist | undefined> {
-    return this._artists.find((artist) => artist.id === id);
+    return this._prisma.artist.findUnique({ where: { id } });
+    // return this._artists.find((artist) => artist.id === id);
   }
 
   async createArtist(dto: CreateArtistDto): Promise<Artist> {
     const artist = new Artist(dto);
-    this._artists.push(artist);
-    return artist;
+    return this._prisma.artist.create({ data: artist });
+    // this._artists.push(artist);
+    // return artist;
   }
 
   async updateArtist(id: string, dto: UpdateArtistDto): Promise<Artist> {
-    const artist = await this.getArtistById(id);
-    if (artist) {
-      Object.assign(artist, dto);
-    }
-    return artist;
+    return this._prisma.artist.update({ where: { id }, data: dto });
+    // const artist = await this.getArtistById(id);
+    // if (artist) {
+    //   Object.assign(artist, dto);
+    // }
+    // return artist;
   }
 
   async deleteArtist(id: string): Promise<void> {
-    this._artists = this._artists.filter((artist) => artist.id !== id);
+    await this._prisma.artist.delete({ where: { id } });
+    // this._artists = this._artists.filter((artist) => artist.id !== id);
   }
 }
