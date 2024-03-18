ace9ad371ddb10465c9f3cacb0dcebf1d85f40af
MDz1985 <m_d@mail.uk>
2024-03-18 12:40:49 +0100

    feat: connect album methods to database

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -2,43 +2,57 @@ import { Injectable } from '@nestjs/common';
 import { Album } from './common/interfaces/album.interface';
 import { CreateAlbumDto } from './common/dto/create-album.dto';
 import { UpdateAlbumDto } from './common/dto/update-album.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AlbumsService {
-  private _albums: Album[] = [];
+  // private _albums: Album[] = [];
+
+  constructor(private readonly _prisma: PrismaService) {}
 
   async getAllAlbums(): Promise<Album[]> {
-    return this._albums;
+    return this._prisma.album.findMany();
+    // return this._albums;
   }
 
   async getAlbumById(id: string): Promise<Album | undefined> {
-    return this._albums.find((album) => album.id === id);
+    return this._prisma.album.findUnique({ where: { id } });
+    // return this._albums.find((album) => album.id === id);
   }
 
   async createAlbum(dto: CreateAlbumDto): Promise<Album> {
     const album = new Album(dto);
-    this._albums.push(album);
-    return album;
+    return this._prisma.album.create({ data: album });
+    // this._albums.push(album);
+    // return album;
   }
 
   async updateAlbum(id: string, dto: UpdateAlbumDto): Promise<Album> {
-    const album = await this.getAlbumById(id);
-    if (album) {
-      Object.assign(album, dto);
-    }
-    return album;
+    return this._prisma.album.update({ where: { id }, data: dto });
+    // const album = await this.getAlbumById(id);
+    // if (album) {
+    //   Object.assign(album, dto);
+    // }
+    // return album;
   }
 
   async deleteAlbum(id: string): Promise<void> {
-    this._albums = this._albums.filter((album) => album.id !== id);
+    await this._prisma.album.delete({ where: { id } });
+    // this._albums = this._albums.filter((album) => album.id !== id);
   }
 
   async removeArtistIdFromAlbum(artistId: string) {
-    this._albums = this._albums.map((album: Album) => {
-      if (album.artistId === artistId) {
-        album.artistId = null;
-      }
-      return album;
+    this._prisma.album.updateMany({
+      where: { artistId },
+      data: {
+        artistId: null,
+      },
     });
+    // this._albums = this._albums.map((album: Album) => {
+    //   if (album.artistId === artistId) {
+    //     album.artistId = null;
+    //   }
+    //   return album;
+    // });
   }
 }
