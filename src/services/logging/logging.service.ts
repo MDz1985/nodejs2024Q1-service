9478fdd425ddfb4d1bc5d6c5d2e7ae54af20adc6
MDz1985 <m_d@mail.uk>
2024-04-01 23:08:39 +0200

    doc: update the README.md and commands

--- src/services/logging/logging.service.ts
+++ src/services/logging/logging.service.ts
@@ -17,7 +17,7 @@ export class LoggingService {
 
   private readonly logger = new Logger(LoggingService.name);
   private readonly logFileSizeLimit =
-    (+process.env.LOG_FILE_SIZE_LIMIT || 1) * 1024;
+    (+process.env.LOG_FILE_SIZE_LIMIT_IN_KB || 1) * 1024;
   private logFileStream: WriteStream;
 
   private get filePath(): string {
@@ -63,37 +63,6 @@ export class LoggingService {
         break;
     }
   }
-
-  // logRequest(request: any, response: any) {
-  //   const logMessage = `Incoming request: ${request.url} - ${JSON.stringify(request.query)} - ${JSON.stringify(request.body)}`;
-  //   this.log(logMessage, LogLevel.INFO);
-  //
-  //   const responseMessage = `Response: ${response.statusCode}`;
-  //   this.log(responseMessage, LogLevel.INFO);
-  // }
-
-  // logError(error: Error) {
-  //   this.logger.error(error.message, error.stack);
-  //   this.logToFile(error.message);
-  // }
-
-  // private logLevel: LogLevel =
-  //   LogLevel[process.env.LOG_LEVEL as keyof typeof LogLevel] || LogLevel.ERROR;
-  //
-  // private shouldLog(level: LogLevel): boolean {
-  //   return level >= this.logLevel;
-  // }
-  //
-  // debug(message: string): void {
-  //   if (this.shouldLog(LogLevel.DEBUG)) {
-  //     process.stdout.write(`DEBUG: ${ message }\n`);
-  //
-  //   }
-  // }
-  //
-  // error(message: string, stack) {
-  //   process.stdout.write(`ERROR: ${message}\n`);
-  // }
 
   private onInit() {
     mkdir(LOG_FOLDER_PATH, { recursive: true }, (err) => {
